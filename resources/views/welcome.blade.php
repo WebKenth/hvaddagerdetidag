1c24e2e3f02625966e828bd0e783e60a3ad1ea23
Martin Knudsen <mkk@webkenth.dk>
2025-05-20 10:28:50 +0200

    imgaes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -236,8 +236,8 @@
         <!-- Middle Section - Images -->
         <section class="middle-section">
             <div class="image-container">
-                <img src="/storage/hat.png" alt="Festhat" class="hat">
-                <img src="/storage/tommy.jpg" alt="Tommy" class="main-image">
+                <img src="/iimages/hat.png" alt="Festhat" class="hat">
+                <img src="/iimages/tommy.jpg" alt="Tommy" class="main-image">
             </div>
         </section>
 
